bcdf0a96d03c7799872884f95e802933f2d6101b
hiya dey sarkar <hiya.deysark@gmail.com>
2021-07-29 13:39:35 +0530

    project added

---
+++ quotes/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: 'The purpose of our lives is to be happy.', author: 'abc'),
+    Quote(text: 'Life is what happens when you’re busy making other plans.', author: 'pqr'),
+    Quote(text: 'Turn your wounds into wisdom.', author: 'xyz'),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quote'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            },
+        )).toList(),
+      ),
+    );
+  }
+}
+
+
